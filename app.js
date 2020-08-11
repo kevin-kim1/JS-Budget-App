b93a66d2cac4e991dfd1a13e993c5693b21e10e6
Kevin Kim <kevinnhkim@live.com>
2020-08-11 16:30:13 -0700

    Created a custom function to maintain dry code

--- app.js
+++ app.js
@@ -17,17 +17,22 @@ var UIController = (function() {
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn').addEventListener('click', function() {
+
+    var ctrlAddItem = function() {
         // 1. Get the field data
         // 2. add the item to budget controller
         // 3. add the item to the UI
         // 4. calculate budget
         // 5. display new budget on the UI
-    });
+        console.log('it works');
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if(event.keycode === 13 || event.which === 13) {
-            console.log('Enter was pressed!');
+            ctrlAddItem();
         }
     });
 })(budgetController, UIController);
