39f8357de8b0a4bfc06e45ceb4b50afe20c41364
Kevin Kim <kevinnhkim@live.com>
2020-08-11 19:11:54 -0700

    in controller module, created a variable to pass the class from UIController to change the input button querySelector string to key value pair of inputBtn

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 var budgetController = (function () {
-    var x = 23;
-    var add = function(a) {
-      return x + a;
-    }
+  var x = 23;
+  var add = function (a) {
+    return x + a;
+  };
 
-    return {
-      publicTest:  function(b) {
-        return (add(b));
-      }
-    }
+  return {
+    publicTest: function (b) {
+      return add(b);
+    },
+  };
 })();
 
-
-var UIController = (function() {
+var UIController = (function () {
   var DOMStrings = {
-    inputType: '.add__type',
-    inputDesc: '.add__description',
-    inputValue: '.add__value'
-  }
-    return {
-              getInput: function() {
-                return {
-                    type: document.querySelector(DOMStrings.inputType).value,
-                    desc: document.querySelector(DOMStrings.inputDesc).value,
-                    value: document.querySelector(DOMStrings.inputValue).value
-                };
-              },
-              getDOMStrings: function () {
-                return DOMStrings;
-              }
-          };
+    inputType: ".add__type",
+    inputDesc: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        desc: document.querySelector(DOMStrings.inputDesc).value,
+        value: document.querySelector(DOMStrings.inputValue).value,
+      };
+    },
+    getDOMStrings: function () {
+      return DOMStrings;
+    }
+  };
 })();
 
-var controller = (function(budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDOMStrings;
-    var ctrlAddItem = function() {
-        // 1. Get the field data
-        var input = UICtrl.getInput();
-        // 2. add the item to budget controller
-        // 3. add the item to the UI
-        // 4. calculate budget
-        // 5. display new budget on the UI
+var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
+  var ctrlAddItem = function () {
+    // 1. Get the field data
+    var input = UICtrl.getInput();
+    // 2. add the item to budget controller
+    // 3. add the item to the UI
+    // 4. calculate budget
+    // 5. display new budget on the UI
+    console.log(input);
+  };
 
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keycode === 13 || event.which === 13) {
+      ctrlAddItem();
     }
-
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        if(event.keycode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-    });
+  });
 })(budgetController, UIController);
